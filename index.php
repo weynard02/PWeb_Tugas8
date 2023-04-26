de51eb48668bc8a254783e27a7bb1fa337e94f1b
Muhammad Razan Athallah <96050618+athraz@users.noreply.github.com>
2023-04-26 16:19:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,39 @@
+<?php
+    session_start();
+    if (!isset($_SESSION['user_id'])){
+        header('Location: login-form.php');
+        exit;
+    }
+?>
+
 <!doctype html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Silaturahmi Online</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>Hello, world!</h1>
+    <h1>Hello, ini landing page</h1>
+    <a class="btn btn-danger" href="proses-logout.php" role="button">Log out</a>
+
+    <?php
+        include('config.php');
+        $uid = $_SESSION['user_id'];
+        $row = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM users WHERE id = '$uid'"));
+        $rid = $row['role_id'];
+        $row = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM roles WHERE id = '$rid'"));
+        if ($row['nama'] != "pejabat"){
+            echo '<a class="btn btn-primary" href="message-form.php" role="button">Add Message</a>';
+        }
+        if ($row['nama'] != "reguler"){
+            echo '<a class="btn btn-info" href="reply-form.php" role="button">Reply Message</a>';
+        }
+    ?>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
