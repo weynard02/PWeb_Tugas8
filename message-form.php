10c636b0e1256888372fe54430b3009c052c7562
weynard02 <samsicoweynard@gmail.com>
2023-04-27 20:22:50 +0700

    feat: style message

--- message-form.php
+++ message-form.php
@@ -25,57 +25,76 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Messsage</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.js"></script>
 </head>
 
 <body>
-    <h1>Add Message</h1>
-    <form action="proses-message.php" method="post">
-        <ul>
+    <div class="container index position-absolute top-50 start-50 translate-middle">
+        <h1 align="center">Add Message</h1>
+        <form action="proses-message.php" method="post">
             <?php
                 include('config.php');
                 $pej = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM roles WHERE nama LIKE 'pejabat'"));
                 $pejabat_id = $pej['id'];
                 $res = mysqli_query($conn, "SELECT * FROM users WHERE id = '$pejabat_id'");
-                echo '<li>';
-                echo '<label for="penerima_user_id">Penerima </label>';
-                echo '<select name="penerima_user_id">';
+                echo '<div class="row mb-3 justify-content-center align-items-center">
+                <div class="col-sm-4">';
+                echo '<label for="penerima_user_id" class="form-label">Penerima </label>';
+                echo '<select name="penerima_user_id" class="form-select">';
                 while ($row = $res->fetch_assoc()){
                     echo "<option value='" . $row['id'] . "'>" . $row['nama'] . "</option>";
                 }
                 echo '</select>';
-                echo '</li>';
+                echo '</div>
+                </div>';
 
                 $res = mysqli_query($conn, "SELECT * FROM types");
-                echo '<li>';
-                echo '<label for="type_id">Tipe Pesan </label>';
-                echo '<select name="type_id">';
+                echo '<div class="row mb-3 justify-content-center align-items-center">
+                <div class="col-sm-4">';
+                echo '<label for="type_id" class="form-label">Tipe Pesan </label>';
+                echo '<select name="type_id" class="form-select">';
                 while ($row = $res->fetch_assoc()){
                     echo "<option value='" . $row['id'] . "'>" . $row['nama'] . "</option>";
                 }
                 echo '</select>';
-                echo '</li>';
+                echo '</div>
+                </div>';
             ?>
 
-            <li>
-                <label for="subject">Subject</label>
-                <input type="text" name="subject" id="subject">
-            </li>
-            <li>
-                <label for="description">Description</label>
-                <input type="text" name="description" id="description">
-            </li>
-
-            <button type="submit" class="btn btn-primary" name="message">Send</button>
-        </ul>
-    </form>
-    <?php
-        if (isset($_SESSION['failed'])) {
-            echo "<div class='alert alert-danger' role='alert'>".$_SESSION['failed']."</div>";
-            unset($_SESSION['failed']);
-        }
-    ?>
-    <a class="btn btn-info" href="index.php" role="button">Back</a>
+            <div class="row mb-3 justify-content-center align-items-center">
+                <div class="col-sm-4">
+                    <label for="subject" class="form-label">Subject</label>
+                    <input type="text" class="form-control" name="subject" id="subject">
+                </div>
+            </div>
+            <div class="row mb-3 justify-content-center align-items-center">
+                <div class="col-sm-4">
+                    <label for="description" class="form-label">Body</label>
+                    <textarea class="form-control" name="description" id="description" style="height: 100px"></textarea>
+                </div>
+            </div>
+            <div class="row mb-3 justify-content-center align-items-center">
+                <div class="col-sm-4">
+                    <a class="btn btn-info" href="index.php" role="button">Back</a>
+                    <button type="submit" class="btn btn-success" name="message">Send</button>
+                </div>
+            </div>
+            <div class="row mb-3 justify-content-center align-items-center">
+                <div class="col-sm-4">
+                <?php
+                    if (isset($_SESSION['failed'])) {
+                        echo "<div class='alert alert-danger' role='alert'>".$_SESSION['failed']."</div>";
+                        unset($_SESSION['failed']);
+                    }
+                ?>
+                </div>
+            </div>
+        </form>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
 </body>
 
 </html>
