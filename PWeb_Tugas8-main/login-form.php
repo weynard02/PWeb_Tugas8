5e086c7072c4711d9a0d06168894e708b2516252
Yoyo999111222 <106955551+Yoyo999111222@users.noreply.github.com>
2023-04-30 02:04:52 +0700

    Update login-form.php

--- PWeb_Tugas8-main/login-form.php
+++ PWeb_Tugas8-main/login-form.php
@@ -15,7 +15,6 @@
     <title>login form</title>
 
     <link rel="stylesheet" href="css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.js"></script>
 </head>
